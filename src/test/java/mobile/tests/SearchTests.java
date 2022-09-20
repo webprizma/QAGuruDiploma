cd6f2f1c44abc4a66c8133ee9325ca2b067f71b3
Yuri Kulagin <kulagin1987@gmail.com>
2022-09-20 18:02:35 +0300

    added mobile test

---
+++ src/test/java/mobile/tests/SearchTests.java
@@ -0,0 +1,45 @@
+package mobile.tests;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests extends TestBase {
+    @Epic("Wildberries mobile autotests")
+    @Story("Search tests")
+    @Feature("Basic search test")
+    @Tag("search")
+    @Tag("mobile")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Sony PlayStation"
+    })
+    void basicSearchTest(String searchPhrase) {
+        step("Выбрать страну", () ->
+                localeSelectScreen.localeSelectors.findBy(text("Россия")).click());
+
+        step("Пропустить Onboarding", () ->
+                localeSelectScreen.skipButton.click());
+
+        step("Ввести поисковый запрос {0}", () -> {
+            mainScreen.searchButton.click();
+            mainScreen.searchInput.sendKeys(searchPhrase);
+            mainScreen.searchResults.findBy(text(searchPhrase)).click();
+        });
+
+        step("Должны быть результаты поиска", () ->
+                searchScreen.searchResultCards.shouldHave(sizeGreaterThan(0)));
+
+        //todo не ищет по результатам
+        step("Должен быть товар с вхождением поискового запроса {0}", () ->
+                searchScreen.searchResultCardsNames.shouldHave(itemWithText(searchPhrase)));
+    }
+}
